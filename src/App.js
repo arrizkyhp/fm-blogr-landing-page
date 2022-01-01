f212875685f109d39edf876f95ebf3a8976608b1
arrizkyhp <52059831+arrizkyhp@users.noreply.github.com>
2022-01-01 19:29:36 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 
+import Home from "pages/Home";
+import { BrowserRouter } from "react-router-dom";
 import './assets/css/style.css';
-import Home from 'pages/Home'
-import { BrowserRouter as Router, Route } from "react-router-dom";
+
 
 function App() {
   return (
-    <Router basename='/fm-blogr-landing-page'>
-      <Route exact path="/" component={Home} />
-    </Router>
+
+    <BrowserRouter>
+      <Home/>
+    </BrowserRouter>
   );
 }
 
